3c9da643e07d85e0057d2a6609aeb5368d5b9de4
NoppanunY <66986848+NoppanunY@users.noreply.github.com>
2023-03-08 17:14:20 +0700

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -1,5 +1,9 @@
 alert("Alert from github")
 
+function clicked(){
+    alert("Function from github")
+}
+
 (function(){
     kintone.events.on('app.record.index.show', function(event) {
         alert("Alert from github")
